6e854da1ef2d6366757065e9e70059806dc8be84
not_sure <yulii.maievskyi@moonion.com>
2021-11-14 06:22:22 +0200

    Added /parseJSON

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,63 @@
 import express from "express"
 import bodyParser from "body-parser"
 import {currentWeek} from "./utils";
-import {Subject, Account, Division, Lesson, Repeat, LessonType} from './models'
+import {Subject, Account, Division, Lesson} from './models'
 
 const app = express()
 const port = process.env.PORT
 setTimeout(() => app.listen(port))
 
 app.use(bodyParser.json())
+
+app.post('/parseJSON', authUser, async (req, res) => {
+    const data = req.body.data;
+    try {
+        for (const lesson of data) {
+            const dvs = await Division.find({name: lesson.divisions})
+            if (dvs.length == 0) continue;
+
+            let divisions: any = [];
+            for (const dv of dvs)
+                divisions.push(dv._id)
+
+            let lecturers: any = [];
+            for (const name of lesson.lecturers) {
+                let lecturer = await Account.findOne({firstname: name})
+                if (!lecturer) {
+                    lecturer = new Account;
+                    lecturer.firstname = name;
+                    lecturer = await lecturer.save();
+                    console.log('lol', lecturer)
+                }
+                lecturers.push(lecturer._id);
+            }
+
+            let subject = await Subject.findOne({name: lesson.subject})
+            if (!subject) {
+                subject = new Subject;
+                subject.name = lesson.subject;
+                subject = await subject.save();
+                console.log('lols', subject);
+            }
+
+            console.log(`subject ${subject._id} lecturers ${lecturers} divisions ${divisions}`)
+            const ls = new Lesson
+            ls.subject = subject._id
+            ls.lecturers = lecturers
+            ls.divisions = divisions
+            ls.repeat = lesson.weeks
+            ls.room = lesson.room
+            ls.type = lesson.type
+            ls.number = lesson.lesson_num
+            ls.day = lesson.day
+            await ls.save()
+        }
+        res.status(200).send('ok')
+    } catch (e: any) {
+        console.log(e.message)
+        res.status(400).send(e.message)
+    }
+})
 
 app.post('/lesson', authUser, async (req, res) => {
     const data = req.body.data
@@ -29,6 +79,55 @@ app.post('/lesson', authUser, async (req, res) => {
             .then()
 
         res.status(200).send(lesson)
+    } catch (e: any) {
+        res.status(400).send(e.message)
+    }
+})
+
+app.post('/division', authUser, async (req, res) => {
+    const data = req.body.data
+    try {
+        delete data._id
+        const division = (new Division(data)).save()
+
+        res.status(200).send(division)
+    } catch (e: any) {
+        res.status(400).send(e.message)
+    }
+})
+
+app.post('/subject', authUser, async (req, res) => {
+    const data = req.body.data
+    try {
+        delete data._id
+
+        const subject = await (new Subject(data)).save()
+
+        res.status(200).send(subject)
+    } catch (e: any) {
+        res.status(400).send(e.message)
+    }
+})
+
+app.post('/account', authUser, async (req, res) => {
+    const data = req.body.data
+    try {
+        let id = data._id
+        delete data._id
+
+        if(id) {
+            await Account.findById(id).update(data)
+        } else {
+            id = (await (new Account(data)).save())._id
+        }
+
+        const account = await Account
+            .findById(id)
+            .populate('lecturerData')
+            .populate('telegramData')
+            .then()
+
+        res.status(200).send(account)
     } catch (e: any) {
         res.status(400).send(e.message)
     }
@@ -99,30 +198,6 @@ app.get('/lesson/:id', async (req, res) => {
         console.log(e.message)
         res.status(400).send()
     }
-})
-
-app.get('/', async (req, res) => {
-    const lecturer = await (new Account({
-        firstname: 'Mykola',
-        lastname: 'Hodowitcher',
-        lecturerData: {degree: 'doctor'},
-    })).save()
-    const division = await (new Division({name: 'UI191', course: 3})).save()
-    const subject = await (new Subject({name: 'OOP'})).save()
-    const lesson = await new Lesson({
-        subject: subject._id,
-        divisions: [division._id],
-        lecturers: [lecturer._id],
-        type: LessonType.Lecture,
-        comment: 'Перенесем на стрим',
-        link: 'google.com',
-        weeks: Repeat.Odd,
-        number: 1,
-        day: 1
-    }).save()
-    console.log(lesson)
-
-    res.send('Hello World')
 })
 
 //todo
