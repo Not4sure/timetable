4284c92f00a8f33f905bbcdacf6848776c6a21ca
not_sure <yulii.maievskyi@moonion.com>
2021-11-27 01:44:35 +0200

    Added basic telegram login check

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,12 +1,18 @@
 import ApiError from '../exceptions/api-error'
-import express from "express";
+import express from 'express';
+import {TelegramLogin} from 'node-telegram-login'
+
+const tgToken = process.env.TG_TOKEN ?? ''
+const tgLoginChecker = new TelegramLogin(tgToken)
 
 export default function (req: express.Request, res: express.Response, next: express.NextFunction) {
   try {
-    const user = req.body.tgUser
-    if (user !== 'admin') {
+    const userData = req.body.tgLogin
+    if(!userData)
       return next(ApiError.UnauthorizedError())
-    }
+
+    if(!tgLoginChecker.checkLoginData(userData))
+      return next(ApiError.UnauthorizedError())
 
     next()
   } catch (e) {
