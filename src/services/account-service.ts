95177f4bb6d7a413fd3796c4559a5e3fd9728a99
notsure <yulii.maievskyi@waterfall.foundation>
2022-03-13 21:01:15 +0200

    fixed account service

--- src/services/account-service.ts
+++ src/services/account-service.ts
@@ -15,7 +15,9 @@ class AccountService {
     }
 
     async login(telegramId: string, telegramData: any) {
+        console.log('id', telegramId)
         let account = await Account.findOne({ telegramData: {id: telegramId}})
+        console.log('account', account)
         if(!account)
             account = await (new Account({ telegramData })).save()
         return account
